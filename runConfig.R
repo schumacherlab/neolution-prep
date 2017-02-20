d4190516cba9d005431e88fd5e12f45f0acd583d
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-02-20 16:13:34 +0100

    update runConfig with new options
    samtools

--- runConfig.R
+++ runConfig.R
@@ -1,29 +1,33 @@
 ## set runtime configuration
-runOptions = list(general = c(),
-                  varcontext = c(),
-                  neolution = c(),
-                  snpeff = c())
+runOptions = list(general = list(),
 
-# set varcontext options
-runOptions$varcontext$numberOfWorkers = 10
+									# set samtools mpileup options
+									samtools = list(numberOfWorkers = 8,
+																	fastaGenomeRef = '/home/NFS/users/l.fanchi/resources/ensembl_87/fasta_dna/Homo_sapiens.GRCh38.87.dna.primary_assembly.fa'),
 
-runOptions$varcontext$fieldSeparator = '"\t"';
-runOptions$varcontext$canonicalOnly = FALSE;
-runOptions$varcontext$peptideContext = FALSE;
-runOptions$varcontext$nmdStatus = TRUE;
+									# set varcontext options
+									varcontext = list(numberOfWorkers = 10,
 
-runOptions$varcontext$varcontextDirectory = '/home/NFS/users/l.fanchi/dev_environments/varcontext'
-runOptions$varcontext$ensemblApi = '/home/NFS/users/l.fanchi/libs/ensembl_81/'
-runOptions$varcontext$perlLibs = paste('/home/NFS/users/l.fanchi/perl5/lib/perl5','/home/NFS/users/l.fanchi/libs/bioperl-live',
-																			 '/home/NFS/users/l.fanchi/libs/ensembl/modules','/home/NFS/users/l.fanchi/libs/ensembl-variation/modules', sep = ':')
+																		fieldSeparator = '"\t"',
+																		canonicalOnly = FALSE,
+																		peptideContext = FALSE,
+																		nmdStatus = TRUE,
 
-# set neolution options
-runOptions$neolution$numberOfWorkers = 3 # number of threads for predictions (each thread spawns 16 children during self-sim checking)
+																		varcontextDirectory = '/home/NFS/users/l.fanchi/dev_environments/varcontext',
+																		ensemblApi = '/home/NFS/users/l.fanchi/libs/ensembl_81/',
+																		perlLibs = paste('/home/NFS/users/l.fanchi/perl5/lib/perl5','/home/NFS/users/l.fanchi/libs/bioperl-live',
+																										 '/home/NFS/users/l.fanchi/libs/ensembl/modules','/home/NFS/users/l.fanchi/libs/ensembl-variation/modules', sep = ':')),
 
-runOptions$neolution$rankCutoff = 3
-runOptions$neolution$processingCutoff = 0.5
-runOptions$neolution$xmer = c(9:11)
+									# set neolution options
+									neolution = list(numberOfWorkers = 3, # number of threads for predictions (each thread spawns 16 children during self-sim checking)
 
-# set snpEff locations
-runOptions$snpeff$path = '/home/NFS/users/l.fanchi/libs/snpEff'
-runOptions$snpeff$build = 'GRCh38.81'
+																	 rankCutoff = 3,
+																	 processingCutoff = 0.5,
+																	 xmer = c(9:11)),
+
+									# set snpEff locations
+									snpeff = list(path = '/home/NFS/users/l.fanchi/libs/snpEff',
+																build = 'GRCh38.81'))
+
+
+
