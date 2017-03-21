2fea32286bef3efdab880f43288f9095fa3264c1
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-03-21 18:33:54 +0100

    update defaults

--- prepareNeolutionInput.R
+++ prepareNeolutionInput.R
@@ -12,13 +12,15 @@ source('./runConfig.R')
 parseAndExtractFieldsFromVcf()
 
 # find evidence for variants in RNAseq data (skipped if no RNAseq data is found)
-findRnaReadLevelEvidenceForVariants()
+findRnaReadLevelEvidenceForVariants(quant_mode = 'salmon')
 
 # run variant context generation
 performVarcontextGeneration()
 
 # prepare Neolution input files (also allows optional merging with RNAseq data; skipped if no RNAseq data found)
-prepareNeolutionInput()
+prepareNeolutionInput(rna_path = '1b_rnaseq_data/processed_salmon',
+											rna_file_suffix = 'salmon-quant-by-ensg\\.tsv',
+											expression_unit = 'tpm')
 
 # generate SnpEff output
 runSnpEff()
