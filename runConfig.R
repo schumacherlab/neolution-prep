9f5df645e309bdd975a05eef590268094f52345c
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-04-03 12:46:56 +0200

    update seqdata regex

--- runConfig.R
+++ runConfig.R
@@ -6,7 +6,7 @@ regexPatterns = list(file_extension = '\\.[^.]+$', # match file extension (every
 										 gs_identifier = 'gs\\d+', # for matching snps not found in dbSNP, keep boundless (no '^' or '$')
 										 rs_identifier = 'rs\\d+', # for matching snps found in dbSNP, keep boundless (no '^' or '$')
 										 cosmic_identifier = 'COSM\\d+', # for matching variants found in COSMIC coding muts database, keep boundless
-										 seqdata_prefix = '_mg.+|_S\\d_L.+', # for isolating GCF prefix
+										 seqdata_prefix = '_mg.+|_S\\d_L.+|-ra-bq.+', # for isolating GCF prefix
 										 allele_exclusion = 'C[0-9]{4}') # for excluding particular alleles from analysis
 
 # run options
