b3eee83e520b2b194088e2dc1ca1aea19ef52b32
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-08-25 18:02:19 +0200

    update GCF prefix regex

--- runConfig.R
+++ runConfig.R
@@ -6,7 +6,7 @@ regexPatterns = list(file_extension = '\\.[^.]+$', # match file extension (every
                      gs_identifier = 'gs\\d+', # for matching germline snps annotated by our pipeline, keep boundless (no '^' or '$')
                      rs_identifier = 'rs\\d+', # for matching snps found in dbSNP, keep boundless (no '^' or '$')
                      cosmic_identifier = 'COSM\\d+', # for matching variants found in COSMIC coding muts database, keep boundless
-                     seqdata_prefix = '(?<=[ATCG]{8}).+', # for isolating GCF prefix
+                     seqdata_prefix = '(?<=[ATCG]{7,8}).+', # for isolating GCF prefix
                      allele_exclusion = 'C[0-9]{4}') # for excluding particular alleles from analysis
 
 # run options
