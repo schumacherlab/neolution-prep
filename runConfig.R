351b09734cececc8621b3dc157bccfc87e37bd9b
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-12-18 14:55:59 +0100

    update paths

--- runConfig.R
+++ runConfig.R
@@ -10,20 +10,19 @@ regexPatterns = list(file_extension = '\\.[^.]+$', # match file extension (every
                      allele_exclusion = 'C[0-9]{4}') # for excluding particular alleles from analysis
 
 # run options
-userPaths = list(libs_path = '/DATA/users/l.fanchi/libs',
-                 resources_path = '/DATA/users/l.fanchi/resources',
-                 home_path = '/home/l.fanchi')
+commonPaths = list(libs_path = '/DATA/users/l.fanchi/libs',
+                   resources_path = '/DATA/resources')
 
-runOptions = list(general = list(gtf_annotation = file.path(userPaths$resources_path,
+runOptions = list(general = list(gtf_annotation = file.path(commonPaths$resources_path,
                                                             'ensembl_90/gtf/Homo_sapiens.GRCh38.90.gtf')),
 
                   # set samtools mpileup options
-                  samtools = list(samtoolsPath = file.path(userPaths$libs_path,
+                  samtools = list(samtoolsPath = file.path(commonPaths$libs_path,
                                                            'samtools-1.5/bin/samtools'),
-                                  sambambaPath = file.path(userPaths$libs_path,
+                                  sambambaPath = file.path(commonPaths$libs_path,
                                                            'sambamba-0.6.6-Linux_x86_64/sambamba'),
                                   numberOfWorkers = 10,
-                                  fastaGenomeRef = file.path(userPaths$resources_path,
+                                  fastaGenomeRef = file.path(commonPaths$resources_path,
                                                              'ensembl_88/fasta_dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa')),
 
                   # set varcontext options
@@ -41,14 +40,14 @@ runOptions = list(general = list(gtf_annotation = file.path(userPaths$resources_
                                     protein_context = TRUE,
                                     nmd_status = TRUE,
 
-                                    varcontext_directory = file.path(userPaths$home_path, 'stable_environments/varcontext'),
-                                    ensembl_api = file.path(userPaths$libs_path, 'ensembl_90/'),
-                                    perl_libs = paste(c(file.path(userPaths$libs_path,
+                                    varcontext_directory = file.path(commonPaths$resources_path, 'pipeline/varcontext'),
+                                    ensembl_api = file.path(commonPaths$libs_path, 'ensembl_90/'),
+                                    perl_libs = paste(c(file.path(commonPaths$libs_path,
                                                                  c('perl5/lib/perl5', 'bioperl-live'))),
                                                      collapse = ':')),
 
                   # set neolution options
-                  neolution = list(path = file.path(userPaths$home_path, 'stable_environments/neolution-live'),
+                  neolution = list(path = file.path(commonPaths$resources_path, 'pipeline/neolution-live'),
                                    numberOfWorkers = 3, # number of sample threads for predictions
                                                         # (each thread spawns 'machine_cores/6' children during self-sim checking)
 
@@ -57,17 +56,14 @@ runOptions = list(general = list(gtf_annotation = file.path(userPaths$resources_
                                    model_cutoff = 0.02, # use 0.01 for TIL screens
                                                         # (more inclusive; we picked up low magnitude TIL hits at low prob scores)
                                                         # use 0.02 for PBMC screens
-                                                        # (more stringent; unlikely to pick up low magnitude responses with low prob scores)
+                                                        # (more stringent; we're unlikely to pick up low magnitude responses with low prob scores)
                                    expression_cutoff = 0,
                                    random_forest_model = TRUE,
                                    xmer = c(9:11),
-                                   selfsim_filter = FALSE,
+                                   selfsim_filter = TRUE,
                                    selfsim_filter_mode = 'simple',
                                    selflist = FALSE),
 
                   # set snpEff locations
-                  snpeff = list(path = file.path(userPaths$libs_path, 'snpEff'),
+                  snpeff = list(path = file.path(commonPaths$libs_path, 'snpEff'),
                                 build = 'GRCh38.86'))
-
-
-
